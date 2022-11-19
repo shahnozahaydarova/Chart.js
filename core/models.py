b1357b7b4fe9f758b42df22a9c12ebdb7494edfb
shahnozahaydarova <shahnozahaydarova03@gmail.com>
2022-11-19 23:32:43 +0500

    examp

--- core/models.py
+++ core/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Post(models.Model):
     class Meta:
-        verbose_name = 'postlarst'
+        verbose_name = 'posttt'
         verbose_name_plural = 'postlar'
     nomi = models.CharField(max_length=50)
     daromadi = models.IntegerField()
     def __str__(self):
-        return self.nomi ,self.daromadi
+        return self.nomi 
